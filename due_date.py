3e2724c8ea007ee711593d4af731e3d767e2d512
Jameel Khan <jkhan11@jh.edu>
2021-02-23 13:43:43 -0500

    Add files via upload

---
+++ due_date.py
@@ -0,0 +1,13 @@
+# Part 2 of week 1 assignment
+
+print("Enter the date the first assignment is due below:")
+day = input("Day (enter 2-digit value): ")
+month = input("Month (enter 2-digit value): ")
+year = input("Year (enter 4-digit value): ")
+
+print("For the time, enter the 2-digit value for each line below:")
+timeHours = input("Enter the hours: ")
+timeMinutes = input("Enter the minutes: ")
+
+print(f"Module 1 Assignment is due on {month}/{day}/{year} " +
+      f"at {timeHours}:{timeMinutes} EST.")
